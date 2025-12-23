dca700f542bb8df65975ab7332322408e947d578
MaiHien-Art <maivanhienspmtk4@gmail.com>
2025-12-23 14:20:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -191,7 +191,7 @@ st.markdown("""
 st.markdown("""
 <div class="main-header">
     <h1>📘 TRỢ LÝ SOẠN GIÁO ÁN TỰ ĐỘNG (NLS)</h1>
-    <p>Tác giả: Lê Mạnh Linh - Trường Tiểu học Hùng Thắng - ĐT: 0989 356 562</p>
+    <p>Tác giả: Mai Văn Hiển - Trường Tiểu học Hùng Thắng - ĐT: 0974 829 859</p>
 </div>
 """, unsafe_allow_html=True)
 
@@ -347,4 +347,4 @@ if st.button("🚀 SOẠN GIÁO ÁN NGAY"):
 
 # --- CHÂN TRANG ---
 st.markdown("---")
-st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Lê Mạnh Linh - Trường Tiểu học Hùng Thắng - ĐT: 0989 356 562</div>", unsafe_allow_html=True)
+st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Mai Văn Hiển - Trường Tiểu học Hùng Thắng - ĐT: 0974 829 859</div>", unsafe_allow_html=True)
